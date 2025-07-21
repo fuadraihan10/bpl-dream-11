8011474768105293194c82c83319ba0f74082577
fuadraihan10 <contact.fuadraihan@gmail.com>
2025-07-21 12:05:54 +0600

    added nav-bar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Nav from './components/nav/nav.jsx'
+
+function App() {
+
+  return (
+    <>
+    {/*---------- nav-bar -------------- */}
+      <Nav></Nav>
+    </>
+  )
+}
+
+export default App
